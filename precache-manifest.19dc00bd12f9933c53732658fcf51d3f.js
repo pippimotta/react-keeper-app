31c0d56420fc3ad6b5b7e8b4f3572c362fc8f94a
Ling-Yi <kinoko@Ling-YideMacBook-Air.local>
2022-03-23 14:38:50 +0900

    Updates

---
+++ precache-manifest.19dc00bd12f9933c53732658fcf51d3f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e179d0eda5f72b4091deb1ef9d9aa112",
+    "url": "/react-keeper-app/index.html"
+  },
+  {
+    "revision": "bd0e949dd698baacb8c1",
+    "url": "/react-keeper-app/static/js/2.4030e18b.chunk.js"
+  },
+  {
+    "revision": "69c1ae61b11500bddf5a",
+    "url": "/react-keeper-app/static/js/main.4dd861c1.chunk.js"
+  },
+  {
+    "revision": "2b2f64b2a76837f056f8",
+    "url": "/react-keeper-app/static/js/runtime-main.c57fe059.js"
+  }
+]);
